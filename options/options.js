84fced567e663fd47b5f650e75823181b6381921
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 08:09:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-scene-dev-104273",
+  url: "https://black-scene-dev-104273.botics.co",
+  api: "https://black-scene-dev-104273.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
